8d2fe024263cb294aeea073dbd6395465105dec2
xiazhao9406 <63313765+xiazhao9406@users.noreply.github.com>
2020-07-17 15:39:48 -0700

    creat a window

---
+++ 02_creatWindows/02_creatWindows.cpp
@@ -0,0 +1,91 @@
+﻿/* -------------------------------------------------------------------
+                    MyWindows.c --Creat a new window but user cannot do any oprations
+                               
+--------------------------------------------------------------------*/
+
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+    // the name of my window
+    static TCHAR szAppName[] = TEXT("MyWindows");
+    HWND hwnd;
+    MSG msg;
+
+    // window struct 
+    WNDCLASS wndclass;
+
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    // the color of my window
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    // the name of my window
+    wndclass.lpszClassName = szAppName;
+
+    if (!RegisterClass(&wndclass))
+    {
+        MessageBox(NULL, TEXT("this project should run in Windows NT！"), szAppName, MB_ICONERROR);
+        return 0;
+    }
+    // The function of Creat windows
+    hwnd = CreateWindow(szAppName,
+        TEXT("Hello, world!"),
+        // cannot do any oprations to the windows
+        WS_DISABLED,// change the third argument in the function
+        // add horizonlrow and vertical row
+        //WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL,
+        
+        
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+// the function of callback.
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    RECT rect;
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hwnd, &ps);
+        GetClientRect(hwnd, &rect);
+        DrawText(hdc, TEXT("hello, this is my frist window！"), -1, &rect,
+            DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+        EndPaint(hwnd, &ps);
+        return 0;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, message, wParam, lParam);
+    
+}
